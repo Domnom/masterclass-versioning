bff41d803a51270ea08fa6d30915f83b35340036
Dominic Trang <dominic_t@hotmail.co.nz>
2020-09-23 16:45:28 +1200

    feat(Creature.schema.ts): Add a constructor and default the creature to living at home

--- Creature.schema.ts
+++ Creature.schema.ts
@@ -1,4 +1,15 @@
+/**
+ * @import Enums
+ */
+import { LOCATIONS } from './location.enum';
+
 class Creature
 {
     name: string;
+    location: LOCATIONS;
+
+    constructor()
+    {
+        this.location = LOCATIONS.HOME;
+    }
 }
